9995846e7f7ac8be0fce8b431b3753f11632dada
Lakshmi Rajeswara Rao. Pamarthi <Lakshmi_rajeswara_rao.Pamarthi@Dell.com>
2025-07-13 19:06:32 +0530

    modified

--- src/oca/CommentedCodeCompiles.java
+++ src/oca/CommentedCodeCompiles.java
@@ -4,7 +4,6 @@ public class CommentedCodeCompiles {
 
 	public static void main(String... args) {
 		System.out.println("Hi Guys!");
-		//Character myChar = new Character('\u000d');
    }
 }
 
---
+++ src/practice/RandomTest.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Random;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+
+		Random random = new Random();
+		System.out.println(random.nextInt(11 ));
+		
+	}
+
+}
